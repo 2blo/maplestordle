3c89a04ab76739a6feabf40151ea1052cee5c90d
2blo <2blo.dev@gmail.com>
2024-12-15 12:34:55 +0100

    add mob and mob_color

--- recreate_database.js
+++ recreate_database.js
@@ -1,7 +1,7 @@
 import { sql } from "drizzle-orm";
 import { drizzle } from "drizzle-orm/node-postgres";
-import * as readline from 'node:readline/promises';
-import { stdin as input, stdout as output } from 'node:process';
+import * as readline from "node:readline/promises";
+import { stdin as input, stdout as output } from "node:process";
 import { env } from "~/env";
 
 const admin_db = drizzle(env.DATABASE_HOST + "/" + env.ADMIN_DATABASE);
@@ -9,34 +9,41 @@ const admin_db = drizzle(env.DATABASE_HOST + "/" + env.ADMIN_DATABASE);
 const rl = readline.createInterface({ input, output });
 
 async function recreateDatabase() {
-    const answer = await rl.question(`This script will DROP and create the operational database ${env.DATABASE}, are you sure you want to continue? (yes/no) `);
-    if (answer.toLowerCase() === 'yes') {
-        console.log('Continuing...');
+  const answer = await rl.question(
+    `This script will DROP and create the operational database ${env.DATABASE}, are you sure you want to continue? (yes/no) `,
+  );
+  if (answer.toLowerCase() === "yes") {
+    console.log("Continuing...");
 
-        console.log(
-            await admin_db.execute(
-                sql`DROP DATABASE IF EXISTS ${sql.raw(env.DATABASE)} with (force)`
-            )
-        );
+    console.log(
+      await admin_db.execute(
+        sql`DROP DATABASE IF EXISTS ${sql.raw(env.DATABASE)} with (force)`,
+      ),
+    );
 
-        console.log(
-            await admin_db.execute(
-                sql`CREATE DATABASE ${sql.raw(env.DATABASE)}`
-            )
-        );
-        const operational_db = drizzle(env.DATABASE_HOST + "/" + env.DATABASE);
-        console.log(await operational_db.execute(sql`CREATE SCHEMA IF NOT EXISTS ${sql.raw(env.DATABASE_SCHEMA)}`)
-
-        );
-    } else {
-        console.log('Exiting...');
-    }
-    rl.close();
-    process.exit(0);
+    console.log(
+      await admin_db.execute(sql`CREATE DATABASE ${sql.raw(env.DATABASE)}`),
+    );
+    const operational_db = drizzle(env.DATABASE_HOST + "/" + env.DATABASE);
+    console.log(
+      await operational_db.execute(
+        sql`CREATE SCHEMA IF NOT EXISTS ${sql.raw(env.DATABASE_SCHEMA)}`,
+      ),
+    );
+    console.log(
+      await operational_db.execute(
+        sql`CREATE SCHEMA IF NOT EXISTS ${sql.raw(env.GAME_DATABASE_SCHEMA)}`,
+      ),
+    );
+  } else {
+    console.log("Exiting...");
+  }
+  rl.close();
+  process.exit(0);
 }
 
-recreateDatabase().catch(err => {
-    console.log(env.DATABASE_HOST)
-    console.error('Error:', err);
-    process.exit(1);
+recreateDatabase().catch((err) => {
+  console.log(env.DATABASE_HOST);
+  console.error("Error:", err);
+  process.exit(1);
 });
--- src/server/auth/config.ts
+++ src/server/auth/config.ts
@@ -42,8 +42,8 @@ export const authConfig = {
   providers: [
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET
-    })
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET,
+    }),
     /**
      * ...add more providers here.
      *
